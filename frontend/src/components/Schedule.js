6be0eee2b3043427614ba592014e78124e10c8c1
Tyler Kierstein <35084718+VulshokBersrker@users.noreply.github.com>
2023-04-16 11:11:18 -0400

    New Schedule
    Please install 
    npx -p devextreme-cli devextreme add devextreme-react
    npm install devextreme@22.2 devextreme-react@22.2 --save --save-exact
    before loading this on the new heroku

--- frontend/src/components/Schedule.js
+++ frontend/src/components/Schedule.js
@@ -1,157 +1,185 @@
-import * as React from 'react';
-import Paper from '@mui/material/Paper';
-import { ViewState, EditingState  } from '@devexpress/dx-react-scheduler';
-import {
-  Appointments,
-  AppointmentTooltip,
-  DateNavigator,
-  DragDropProvider,
-  MonthView,
-  Resources,
-  Scheduler,
-  Toolbar,
-  ViewSwitcher,
-  WeekView,
-} from '@devexpress/dx-react-scheduler-material-ui';
+import React from 'react';
+import Scheduler, { Resource } from 'devextreme-react/scheduler';
 
 
+// Pull from database
+const link = 'http://localhost:5000/user/getClasses';
 
+async function getTimes() {
+  let classes = (await getUserInfo('640a38bd0c323e04090300a4','hello'));
+  classes = classes.Classes;
 
+  const times = [];
 
-// Get current date
-const current = new Date();
-const date = `${current.getDate()}/${current.getMonth()+1}/${current.getFullYear()}`;
-const currentDate = {date};
+  for (let i = 0; i < classes.length; i++) {
+    const time = formatTime(classes[i].Times);
+    const obj = {
+      Title: classes[i].Title,
+      Time: time
+    };
+    times.push(obj);
+  }
 
-
-const appointments = [{
-  title: 'COP4331',
-  startDate: new Date("April 16, 2023 13:24"),
-  endDate: new Date("April 16, 2023 15:24"),
-  members: [5, 1, 3],
-  id: 0,
-  classId: 1,
-  rRule: 'FREQ=WEEKLY;COUNT=2',
-},
-{
-  title: 'Test 2',
-  startDate: new Date("April 1, 2023 02:24"),
-  endDate: new Date("April 1, 2023 05:24"),
-  id: 1,
-  classId: 2,
-  members: [5, 1, 3],
-  rRule: 'FREQ=DAILY;COUNT=3',
-},
-{
-  title: 'Test 3',
-  startDate: new Date("March 20, 2023 02:24"),
-  endDate: new Date("March 20, 2023 05:24"),
-  id: 2,
-  classId: 3,
-  rRule: 'FREQ=WEEKLY;COUNT=4',
-},
-{
-  title: 'Test 4',
-  startDate: new Date("April 17, 2023 02:24"),
-  endDate: new Date("April 17, 2023 03:24"),
-  id: 3,
-  classId: 4,
-  rRule: 'FREQ=DAILY;COUNT=2',
+  return times;
 }
-];
 
-// These are the classIDs
+async function getUserInfo (userId, token) {
+  const response = await fetch(`${link}/${userId}/${token}`);
+  const data = await response.json();
+  return data;
+};
+
+function formatTime(string) {
+  const days = {
+    M: 'Monday',
+    T: 'Tuesday',
+    W: 'Wednesday',
+    R: 'Thursday',
+    F: 'Friday',
+    S: 'Saturday',
+    U: 'Sunday'
+  };
+
+  let daysArray = [];
+  let startTime = '';
+  let endTime = '';
+
+  const timeString = string.match(/\d.*\d/)[0];
+  const [start, end] = timeString.split(' - ');
+  startTime = start;
+  endTime = end;
+
+  for (let i = 0; i < string.length; i++) {
+    if (days[string[i]]) {
+      daysArray.push(days[string[i]]);
+    }
+  }
+
+  return {
+    days: daysArray,
+    startTime,
+    endTime
+  };
+}
+  
+let times = [];
+
+(async function() { 
+  times = await getTimes();
+
+  convertFormats();
+})();
+
+
+let CourseCalendar = [];
+
+// Convert the database formats to match the calendar
+function convertFormats() {
+
+  for (let i = 0; i < times.length; i++) {
+    // Convert rRule format
+    let tempRule = 'FREQ=WEEKLY;INTERVAL=1;UNTIL=20230526T000000Z;BYDAY=';
+    for(let j = 0; j < times[i].Time.days.length; j++) {
+      
+      switch(times[i].Time.days[j]) {
+        case 'Monday': 
+          tempRule +='MO';
+          break;
+        case 'Tuesday': 
+          tempRule +='TU';
+          break;
+        case 'Wednesday': 
+          tempRule += 'WE';
+          break;
+        case 'Thursday': 
+          tempRule += 'TH';
+          break;
+        case 'Friday': 
+          tempRule += 'FR';
+          break;
+        case 'Saturday': 
+          tempRule += 'SA';
+          break;
+        case 'Sunday': 
+          tempRule += 'SU';
+          break;
+        default:
+      }
+      tempRule += ',';
+    }
+
+    let editedText = tempRule.slice(0, -1);
+    
+    // Convert startTime and endTime
+    let semesterStart = 'January 9, 2023';
+
+    if(times[i].Time.startTime.slice(-2) === 'PM') {
+      times[i].Time.endTime = semesterStart + " " + times[i].Time.endTime +' PM';
+      times[i].Time.startTime = semesterStart + " " + times[i].Time.startTime +' PM';
+    }
+
+    let obj = { 
+      text: times[i].Title,
+      startDate: new Date(times[i].Time.startTime),
+      endDate: new Date(times[i].Time.endTime),
+      roomId: i,
+      recurrenceRule: editedText, // BYDAY - Every day of the week it happens
+    };
+    CourseCalendar.push(obj);
+  }
+
+  //console.log(CourseCalendar);
+}
+
+
 export const resourcesData = [
   {
     text: 'Room 101',
-    id: 0,
-    color: '#FF5733',
-  }, 
-  {
-    text: 'Room 102',
     id: 1,
-    color: '#33B224',
-  }, 
-  {
-    text: 'Room 103',
+    color: '#bbd806',
+  }, {
+    text: 'Room 102',
     id: 2,
-    color: '#0ECDD6',
-  }, 
-  {
-    text: 'Room 104',
+    color: '#f34c8a',
+  }, {
+    text: 'Room 103',
     id: 3,
-    color: '#D60E29',
-  },
-  {
+    color: '#ae7fcc',
+  }, {
     text: 'Meeting room',
     id: 4,
-    color: '#7E57C1',
+    color: '#ff8817',
+  }, {
+    text: 'Conference hall',
+    id: 5,
+    color: '#03bb92',
   },
 ];
 
+function Schedule()  {
 
+  // Get current date
+  const currentDate = new Date();
+  const views = ['week', 'month'];
 
-
-export default class Demo extends React.PureComponent {
-  constructor(props) {
-    super(props);
-    this.state = {
-      data: appointments,
-      resources: [
-        {
-          fieldName: 'classId',
-          title: 'Room',
-          instances: resourcesData,
-        },
-      ],
-    };
-
-    this.commitChanges = this.commitChanges.bind(this);
-  }
-
-  commitChanges({ added, changed, deleted }) {
-    this.setState((state) => {
-      let { data } = state;
-      if (added) {
-        const startingAddedId = data.length > 0 ? data[data.length - 1].id + 1 : 0;
-        data = [...data, { id: startingAddedId, ...added }];
-      }
-      if (changed) {
-        data = data.map(appointment => (
-          changed[appointment.id] ? { ...appointment, ...changed[appointment.id] } : appointment));
-      }
-      if (deleted !== undefined) {
-        data = data.filter(appointment => appointment.id !== deleted);
-      }
-      return { data };
-    });
-  }
-
-  render() {
-    const { data, resources } = this.state;
-
-    return (
-      <Paper >
-        <Scheduler data={data} height={'auto'} >
-          <ViewState defaultCurrentDate= {currentDate} defaultCurrentViewName="Month"/>
-          <EditingState onCommitChanges={this.commitChanges} />
-          <WeekView startDayHour={6} endDayHour={18} />
-          <MonthView />
-
-          <Toolbar />
-          <DateNavigator />
-          <ViewSwitcher/>
-
-          <Appointments />
-          <AppointmentTooltip
-            showCloseButton
-            showOpenButton
-          />
-          <Resources data={resources} mainResourceName="classId" />
-
-          <DragDropProvider />
-        </Scheduler>
-      </Paper>
-    );
-  }
+  return (
+    <Scheduler
+    timeZone="America/New_York"
+    dataSource={CourseCalendar}
+    views={views}
+    defaultCurrentView="month"
+    defaultCurrentDate={currentDate}
+    startDayHour={9}
+    height={800}
+    >
+    <Resource
+      dataSource={resourcesData}
+      fieldExpr="roomId"
+      label="Room"
+    />
+  </Scheduler>
+  );
+  
 }
+
+export default Schedule;
