0a42d61d71be09e02c1a0a532042150bead47fd2
Tyler Kierstein <35084718+VulshokBersrker@users.noreply.github.com>
2023-04-19 00:11:51 -0400

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,34 @@
+// React Libraries
 import React from 'react';
 import './App.css';
+import 'devextreme/dist/css/dx.light.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+// Routes
 import LoginPage from './pages/LoginPage';
-import CardPage from './pages/CardPage';
+import VerifyEmail from './pages/EmailVerificationPage';
+
 import RegisterPage from './pages/RegisterPage';
+import CoursesPage from './pages/CoursesPage';
+import SchedulePage from './pages/SchedulePage';
+import ResetPasswordPage from './pages/ResetPasswordPage';
 
 function App() {
+
   return (
     <BrowserRouter>
-    <Routes>
-      <Route path="/" index element={<LoginPage />} />
-      <Route path="/cards" index element={<CardPage />} />
-      <Route path="/register" index element={<RegisterPage />} />
-    </Routes>
-  </BrowserRouter>
-);
+      <Routes>
+        <Route path="/" index element={<LoginPage />} />
+        <Route path="/register" index element={<RegisterPage />} />
+        <Route path="/email-verification" index element={<VerifyEmail/>} /> 
+        <Route path="/resetpassword" index element={<ResetPasswordPage />} />
+        <Route path="/courses" index element={<CoursesPage />} />
+        <Route path="/schedule" index element={<SchedulePage/>} />
+      </Routes>
+    </BrowserRouter>
+  );
+  
 }
 
 export default App;
